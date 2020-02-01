4da188d3ddf41c61fd70b94e1b061db72db6a4d3
MathuraveeraganeshMe <MathuraveeraganeshMe@10.0.75.1>
2020-02-01 19:24:13 +0530

    Jenkins Deployment

---
+++ src/main/java/com/leafBot/pages/LoginPage.java
@@ -0,0 +1,64 @@
+package com.leafBot.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+import com.aventstack.extentreports.ExtentTest;
+import com.leafBot.testng.api.base.ProjectSpecificMethods;
+
+public class LoginPage extends ProjectSpecificMethods{
+	
+	public LoginPage(RemoteWebDriver driver, ExtentTest node, ExtentTest test){
+		this.driver = driver;
+		this.node = node;
+		this.test = test;
+		PageFactory.initElements(driver,this);
+	}
+	 
+	@FindBy(how=How.ID,using="username")
+	private WebElement eleUserName;	
+	
+	@FindBy(how=How.ID,using="password")
+	private WebElement elePassword;	
+	
+	@FindBy(how=How.CLASS_NAME,using="decorativeSubmit")
+	private WebElement eleLogin;
+	
+	@FindBy(how=How.ID, using = "errorDiv")
+	private WebElement eleVerifyErrMsg;
+	
+	
+	//@Given("Enter the username as (.*)")
+	public LoginPage enterUserName(String data) {	
+		clearAndType(eleUserName, data);
+		return this;
+	}	
+
+	//@Given("Enter the Password as (.*)")
+	public LoginPage enterPassword(String data) {
+		clearAndType(elePassword, data);
+		return this;
+	}	
+	
+	//@Given("Click on the Login")
+	public HomePage clickLogin() {
+		click(eleLogin);
+		return new HomePage(driver, node, test);		
+	}
+	
+	public LoginPage clickLogInForFailer() {
+		click(eleLogin);
+		return this;		
+	}
+	
+	//@Given("Verify the error message (.*)")
+	public LoginPage verifyErrorMsg(String data) {
+		verifyPartialText(eleVerifyErrMsg, data);
+		return this;
+	}
+	
+	
+}
